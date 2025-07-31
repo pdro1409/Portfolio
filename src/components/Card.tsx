78d6ba59f1fd8191bb98bfba49cfe4ac3afff1bb
Pedro Augusto <panuness1010@gmail.com>
2025-07-31 18:46:24 -0300

    add new button, add new icon

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,15 +5,32 @@ export const Card = () => {
     <>
       {projects.map((project) => (
         <div
-          className="bg-[#050505] py-4 px-6 border border-white rounded-md"
+          className="bg-[#050505] py-4 px-6 border border-[#3a0081] rounded-md transform transition-transform duration-300 hover:scale-110"
           key={project.id}
         >
           <div>
             <img src={project.image} alt="" className="w-full" />
           </div>
-          <div>
-            <h3>{project.title}</h3>
-            <p>{project.description}</p>
+          <div className="flex flex-col gap-2 mt-4  justify-center">
+            <h3 className="font-semibold text-2xl">{project.title}</h3>
+            <p className="font-medium text-base">{project.description}</p>
+            <div className="flex gap-4 mt-4">
+              <a href={project.link}>
+                <img
+                  src="/assets/git-icon.svg"
+                  alt=""
+                  className="transform transition-transform duration-300 hover:scale-110"
+                />
+              </a>
+
+              <a href={project.link}>
+                <img
+                  src="/assets/preview-icon.svg"
+                  alt=""
+                  className="transform transition-transform duration-300 hover:scale-110"
+                />
+              </a>
+            </div>
           </div>
         </div>
       ))}
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,42 +2,48 @@ export const projects = [
   {
     id: 1,
     title: "Pmovie",
-    description: "Projeto de gerenciamento de filmes.",
+    description:
+      "App em React que exibe filmes populares da API TMDb com design responsivo, efeitos hover e visual limpo usando Tailwind CSS.",
     image: "/assets/project-1.png",
     link: "https://pmovies-rosy.vercel.app/",
   },
   {
     id: 2,
     title: "Pmovie",
-    description: "Projeto de gerenciamento de filmes.",
+    description:
+      "App em React que exibe filmes populares da API TMDb com design responsivo, efeitos hover e visual limpo usando Tailwind CSS.",
     image: "/assets/project-1.png",
     link: "https://pmovies-rosy.vercel.app/",
   },
   {
     id: 3,
     title: "Pmovie",
-    description: "Projeto de gerenciamento de filmes.",
+    description:
+      "App em React que exibe filmes populares da API TMDb com design responsivo, efeitos hover e visual limpo usando Tailwind CSS.",
     image: "/assets/project-1.png",
     link: "https://pmovies-rosy.vercel.app/",
   },
   {
     id: 4,
     title: "Pmovie",
-    description: "Projeto de gerenciamento de filmes.",
+    description:
+      "App em React que exibe filmes populares da API TMDb com design responsivo, efeitos hover e visual limpo usando Tailwind CSS.",
     image: "/assets/project-1.png",
     link: "https://pmovies-rosy.vercel.app/",
   },
   {
     id: 5,
     title: "Pmovie",
-    description: "Projeto de gerenciamento de filmes.",
+    description:
+      "App em React que exibe filmes populares da API TMDb com design responsivo, efeitos hover e visual limpo usando Tailwind CSS.",
     image: "/assets/project-1.png",
     link: "https://pmovies-rosy.vercel.app/",
   },
   {
     id: 6,
     title: "Pmovie",
-    description: "Projeto de gerenciamento de filmes.",
+    description:
+      "App em React que exibe filmes populares da API TMDb com design responsivo, efeitos hover e visual limpo usando Tailwind CSS.",
     image: "/assets/project-1.png",
     link: "https://pmovies-rosy.vercel.app/",
   },
